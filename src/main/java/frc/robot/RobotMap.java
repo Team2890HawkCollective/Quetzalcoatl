282928eb81c99848a2d9bdff7ec2cee92d9daf02
Team2890 <2890FRC@gmail.com>
2019-02-06 14:58:53 -0500

    Changed port id of assistant driver controller to port 2

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -34,7 +34,7 @@ public class RobotMap {
 
   //PORT IDS
   public static final int DRIVER_CONTROLLER_PORT = 0;
-  public static final int ASSISTANT_DRIVER_CONTROLLER_PORT = 1;
+  public static final int ASSISTANT_DRIVER_CONTROLLER_PORT = 2;
   public static final int LEFT_DRIVER_JOYSTICK_PORT = 0;
   public static final int RIGHT_DRIVER_JOYSTICK_PORT = 1;
   public static final int LEFT_FRONT_TALON_ID = 1;
